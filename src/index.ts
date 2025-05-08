2b408bc79f468e96cf21d91338faf64135541110
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-08 20:27:35 +0600

    Problem 2 added.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ function formatString(input: string, toUpper?: boolean): string {
         return input.toUpperCase();
     }
 }
+
+function filterByRating(
+    items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+    const filteredItems = items.filter((item) => item.rating >= 4);
+    return filteredItems;
+}
